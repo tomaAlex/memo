58aa9b76bf8d96af86cf1f3cb2f248d99d5c126b
Alexandru-Eugen Toma <eugen@razorsecure.com>
2023-01-17 00:03:54 +0000

    style cashout supplier button

--- src/screens/Main/Settings/CashOutSheet/CashOutSupplierButton/index.tsx
+++ src/screens/Main/Settings/CashOutSheet/CashOutSupplierButton/index.tsx
@@ -1,6 +1,8 @@
 import React, { useState } from "react";
 import { Text, TouchableOpacity } from "react-native";
 import CashOutSupplierForm from "./CashOutSupplierForm";
+import styles from "./CashOutSupplierButton.module.scss";
+import { useTranslation } from "react-i18next";
 
 type TProps = {
 	setShouldFetchBankAccounts: (shouldFetchBankAccounts: boolean) => void;
@@ -8,11 +10,14 @@ type TProps = {
 
 const CashOutSupplierButton = ({ setShouldFetchBankAccounts }: TProps) => {
 	const [showForm, setShowForm] = useState(false);
+	const [t] = useTranslation("translation", {
+		keyPrefix: "Screens.Main.Settings.CashOutSupplierButton",
+	});
 
 	return (
 		<>
-			<TouchableOpacity onPress={() => setShowForm(true)}>
-				<Text>Add new bank account</Text>
+			<TouchableOpacity style={styles.container} onPress={() => setShowForm(true)}>
+				<Text style={styles.container__text}>{t("addBankAccount")}</Text>
 			</TouchableOpacity>
 			<CashOutSupplierForm
 				visible={showForm}
