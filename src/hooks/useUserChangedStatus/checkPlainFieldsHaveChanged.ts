459c66f42554bd7238328ac7c402757097fdef99
Alexandru-Eugen Toma <eugen@razorsecure.com>
2023-02-24 17:46:25 +0000

    dynamically display the settings update button

--- src/hooks/useUserChangedStatus/checkPlainFieldsHaveChanged.ts
+++ src/hooks/useUserChangedStatus/checkPlainFieldsHaveChanged.ts
@@ -1,14 +1,19 @@
 import { User } from "types/index";
+import checkStringifiedFieldsHaveChanged from "./checkStringifiedFieldsHaveChanged";
 
 const checkPlainFieldsHaveChanged = (user: User, possiblyChangedUser: User): boolean => {
 	type UserKey = keyof User;
 	type UserKeys = UserKey[];
-	const specialKeysOfVerification = ["coordinates", "features", "searchFilters"] as UserKeys;
+	const specialKeysOfVerification = ["searchFilters"] as UserKeys;
 	const plainKeysOfVerification = (Object.keys(user) as UserKeys).filter(
 		(key) => !specialKeysOfVerification.includes(key)
 	);
 	for (const key of plainKeysOfVerification) {
-		if (user[key] !== possiblyChangedUser[key]) return true;
+		const currentUserFieldValue = user[key];
+		const possiblyChangedUserFieldValue = possiblyChangedUser[key];
+		if (checkStringifiedFieldsHaveChanged(currentUserFieldValue, possiblyChangedUserFieldValue)) {
+			return true;
+		}
 	}
 	return false;
 };
--- src/hooks/useUserChangedStatus/index.ts
+++ src/hooks/useUserChangedStatus/index.ts
@@ -3,22 +3,17 @@ import { selectUser } from "redux/selectors";
 import { User } from "types/index";
 import checkPlainFieldsHaveChanged from "./checkPlainFieldsHaveChanged";
 import checkSearchFiltersHaveChanged from "./checkSearchFiltersHaveChanged";
-import checkStringifiedFieldsHaveChanged from "./checkStringifiedFieldsHaveChanged";
 
 export const useUserChangedStatus = () => {
 	const user = useSelector(selectUser);
 
 	return (possiblyChangedUser: User): boolean => {
-		const { coordinates, features, searchFilters } = user;
-		const {
-			coordinates: possiblyChangedCoordinates,
-			features: possiblyChangedFeatures,
-			searchFilters: possiblyChangedSearchFilters,
-		} = possiblyChangedUser;
+		const { searchFilters } = user;
+		const { searchFilters: possiblyChangedSearchFilters } = possiblyChangedUser;
+
 		if (checkPlainFieldsHaveChanged(user, possiblyChangedUser)) return true;
-		if (checkStringifiedFieldsHaveChanged(coordinates, possiblyChangedCoordinates)) return true;
-		if (checkStringifiedFieldsHaveChanged(features, possiblyChangedFeatures)) return true;
 		if (checkSearchFiltersHaveChanged(searchFilters, possiblyChangedSearchFilters)) return true;
+
 		return false;
 	};
 };
