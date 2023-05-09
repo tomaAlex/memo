0d87f117d6bd0e9810979838247d5a72dac851e0
Alexandru-Eugen Toma <eugen@razorsecure.com>
2023-05-09 23:49:59 +0100

    add QR code screen for sharing downloading link with others

--- src/screens/Main/Settings/SettingsPreferences/SettingsPreferencesQrCode/index.tsx
+++ src/screens/Main/Settings/SettingsPreferences/SettingsPreferencesQrCode/index.tsx
@@ -1,16 +1,24 @@
 import React from "react";
-import { Image, TouchableOpacity, View } from "react-native";
+import { Image, Linking, Text, TouchableOpacity, View } from "react-native";
 import connector from "redux/connector";
 import { ScreenProps, SettingsPreferencesScreenNames } from "types";
 import styles from "./SettingsPreferencesQrCode.module.scss";
+import { useTranslation } from "react-i18next";
 
 const SettingsPreferencesQrCode = ({}: ScreenProps<SettingsPreferencesScreenNames.QRCode>) => {
+	const [t] = useTranslation("translation", { keyPrefix: "Screens.Main.Settings.SettingsPreferences.QRCode" });
+
 	return (
 		<View style={styles.container}>
-			{/* <SearchFiltersForm resetRecommendations={resetFilteredRecommendations} /> */}
-			{/* <DeleteAccountButton /> */}
-			<TouchableOpacity style={styles.container__qrCodeContainer} onPress={() => {}}>
+			<TouchableOpacity
+				style={styles.container__qrCodeContainer}
+				onPress={() => {
+					Linking.openURL("https://memodating.com/#download");
+				}}
+			>
+				<Text style={styles.container__qrCodeContainer__caption}>{t("title")} 💓</Text>
 				<Image source={require("./download-qr.png")} style={styles.container__qrCodeContainer__qrCode} />
+				<Text style={styles.container__qrCodeContainer__caption}>{t("caption")}!</Text>
 			</TouchableOpacity>
 		</View>
 	);
